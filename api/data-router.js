f9c9ad54e82808d591368884829e3f2e74c74a61
Jake Grella <jgrella13@gmail.com>
2021-06-06 13:45:01 -0500

    update params

--- api/data-router.js
+++ api/data-router.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const axios = require('axios').default;
 
 // route to whale-alert.io
-router.get('/:minTransactionVal', async (req, res) => {
+router.get('/:currency/:minTransactionVal', async (req, res) => {
+  const { currency, minTransactionVal } = req.params;
   try {
     const response = await axios.get(
-      `https://api.whale-alert.io/v1/transactions?api_key=${process.env.API_KEY}&min_value=${req.minTransactionVal}`
+      `https://api.whale-alert.io/v1/transactions?api_key=${process.env.API_KEY}&currency=${currency}&min_value=${minTransactionVal}`
     );
     console.log(response);
   } catch (err) {
@@ -13,3 +14,5 @@ router.get('/:minTransactionVal', async (req, res) => {
     res.status(500).json({ errMessage: err.message });
   }
 });
+
+module.exports = router;
