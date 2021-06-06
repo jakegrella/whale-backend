8906d59b0819fc818b6c64e0cf50747203085bdc
Jake Grella <jgrella13@gmail.com>
2021-06-06 13:44:53 -0500

    import dataRouter

--- api/server.js
+++ api/server.js
@@ -4,12 +4,12 @@ const helmet = require('helmet');
 
 const server = express();
 
-// add router imports here
+const dataRouter = require('./data-router');
 
 server.use(helmet());
 server.use(cors());
 
-// add router uses here
+server.use('/', dataRouter);
 
 server.get('/', (_, res) => {
   res.json({ api: 'api running' });
